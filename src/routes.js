15b4a577eb149c8196074b1b2961ea1b0016761e
diegoaraujo85 <doasalah@gmail.com>
2020-01-26 18:08:37 -0300

    criando loader de models

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
 
+import User from './app/models/User';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello Oi' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Diego Fernandes',
+    email: 'diego@rockeseat.com.br',
+    password_hash: '1234323423',
+  });
+  return res.json(user);
 });
 
 export default routes;

fc860452b25003bbbc6e6fd22523cbdb8ed0564c
diegoaraujo85 <doasalah@gmail.com>
2020-02-01 07:51:26 -0300

    listagem de providers

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -18,6 +19,8 @@ routes.use(authMiddleware); // por estar declarado aqui, só será aplicado nas 
 
 routes.get('/users', UserController.index);
 routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 

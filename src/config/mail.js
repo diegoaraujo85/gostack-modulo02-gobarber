785c335b15793583a9d790c70f030213d89b2b96
diegoaraujo85 <doasalah@gmail.com>
2020-02-04 13:11:20 -0300

    enviando email de cancelamento

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'd5203d9b876249',
+    pass: 'c2b1c97bf09c97',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
